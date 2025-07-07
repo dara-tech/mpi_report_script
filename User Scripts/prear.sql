0d35d6f743e96df5df205cad64d6864d518c443e
Dara <97618988+dara-tech@users.noreply.github.com>
2025-07-07 10:10:21 +0700

    remove

--- User Scripts/prear.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:39dc3306973b007ea7a20680c1425ccad62796fd1c375a074c3ad7f4ede8231e
-size 1247
--- User Scripts/prearttk.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:997eaf3fcc78a91b57e2a9d121569e4bdbc7650193f5e949290909f59ddf70ae
-size 161839742
